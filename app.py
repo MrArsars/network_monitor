6cd1ce81fc83218452e8c1b5480da1e479409b04
MrArsars <arseniu437@gmail.com>
2024-06-06 11:36:44 +0300

    domen resolving

--- app.py
+++ app.py
@@ -1,49 +1,58 @@
 from flask import Flask, render_template
+from flask_socketio import SocketIO
 from scapy.all import sniff
-from flask_socketio import SocketIO, emit
+from scapy.layers.dns import DNS
 from scapy.layers.inet import IP
 import threading
 from datetime import datetime
-import socket
 
 app = Flask(__name__)
 socketio = SocketIO(app)
-packets_info = []
+
+# Change packets_info to a dictionary
+packets_info = {}
 
 
 class NetworkMonitor:
-    def __init__(self, interface='wlp0s20f3'):
-        self.interface = interface
-
     def packet_handler(self, packet):
         if IP in packet:
             src_ip = packet[IP].src
             dst_ip = packet[IP].dst
+            dst_domen = extract_domain_from_dns(packet)
+
+            # Filter out packets related to local machine to avoid recursive triggers
             if src_ip == "127.0.0.1" or dst_ip == "127.0.0.1":
                 return
-            packet_size = len(packet) / 1024
+
+            packet_size = len(packet) / 1024  # Convert to KB
             timestamp = datetime.now().strftime('%d-%m-%Y %H:%M:%S')
-            #src_domain = self.resolve_domain(src_ip)
-            #dst_domain = self.resolve_domain(dst_ip)
-            packet_info = {'timestamp': timestamp, 'src': src_ip, 'dst': dst_ip, 'size': f"{packet_size:.2f}"}
-            packets_info.append(packet_info)
 
-            if len(packets_info) > 100:  # Keep only the latest 10 packets
-                packets_info.pop(0)
+            # Create a key for aggregation
+            key = (timestamp, src_ip, dst_ip)
+            if key in packets_info:
+                packets_info[key]['size'] += packet_size
+                packets_info[key]['size'] = round(packets_info[key]['size'], 2)
+            else:
+                packets_info[key] = {'timestamp': timestamp, 'src': src_ip, 'dst': dst_ip,
+                                     'size': round(packet_size, 2), 'domen': dst_domen}
 
-    def resolve_domain(self, ip_address):
-        try:
-            return socket.gethostbyaddr(ip_address)[0]
-        except socket.herror:
-            return ip_address
 
     def start_sniffing(self):
-        sniff(iface=self.interface, prn=self.packet_handler, store=False, filter="ip")
+        sniff(prn=self.packet_handler, store=False, filter="ip")
 
     def start(self):
         thread = threading.Thread(target=self.start_sniffing)
         thread.daemon = True
         thread.start()
+
+
+def extract_domain_from_dns(packet):
+    if packet.haslayer(DNS) and packet[DNS].qr == 1:  # Check if the packet is a DNS response
+        for i in range(packet[DNS].ancount):
+            rdata = packet[DNS].an[i].rdata
+            rrname = packet[DNS].an[i].rrname
+            if isinstance(rdata, str) and isinstance(rrname, bytes):
+                return rrname.decode("utf-8")
 
 
 monitor = NetworkMonitor()
@@ -52,22 +61,25 @@ monitor.start()
 
 @app.route('/')
 def index():
-    return render_template('index.html', packets=packets_info)
+    # Convert dictionary to list for rendering
+    packets_list = list(packets_info.values())
+    return render_template('index.html', packets=packets_list)
 
 
 @socketio.on('connect', namespace='/test')
 def test_connect():
-    for packet in packets_info:
-        socketio.emit('packet_update', packet, namespace='/test')
+    packets_list = list(packets_info.values())
+    socketio.emit('packet_update', packets_list, namespace='/test')
 
 
 def send_updates():
     while True:
-        socketio.emit('packet_update', packets_info, namespace='/test')
-        socketio.sleep(2) 
+        packets_list = list(packets_info.values())
+        socketio.emit('packet_update', packets_list, namespace='/test')
+        socketio.sleep(1) 
 
 
 socketio.start_background_task(send_updates)
 
 if __name__ == "__main__":
-    socketio.run(host='0.0.0.0', port=5000)
+    socketio.run(app, host='0.0.0.0', port=5000)
